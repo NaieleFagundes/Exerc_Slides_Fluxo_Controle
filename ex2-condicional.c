2c9f859441c635b92745128d0ab47a2de0bce432
Naiele Fagundes <163013181+NaieleFagundes@users.noreply.github.com>
2025-05-20 16:05:42 -0300

    ex2-condicional.c

--- ex2-condicional.c
+++ ex2-condicional.c
@@ -1,8 +1,3 @@
-//Exercício 2: Calculadora de Operações Básicas
-//Intermediário - switch-case)
-//Crie uma calculadora que leia dois números e um operador (+, -, *, /) e, usando switch-case, realize a operação correspondente.
-//Trate o caso de divisão por zero.
-
 #include <stdio.h>
 int main (){
     float n1, n2;
@@ -12,17 +7,21 @@ int main (){
     scanf ("%f %f %c", &n1, &n2, &operador);
     switch (operador) {
 	 case '+': 
-		printf ("%.2f", n1 + n2);
+		printf ("Resultado: %.2f", n1 + n2);
 		break;
 	 case '-': 
-		printf ("%.2f", n1 - n2);
+		printf ("Resultado: %.2f", n1 - n2);
 		break;
 	 case '*': 
-		printf("%.2f", n1 * n2);
+		printf("Resultado: %.2f", n1 * n2);
 		break;
 	 case '/': 
-		printf("%.2f", n1 / n2);
-		break;
+	      if (n2 == 0) {
+             printf("Erro: divisão por zero não é permitida.\n");
+            } else {
+                printf("Resultado: %.2f", n1 / n2);
+            }
+        break;
 	 default:
 		printf("operador inválido");
 		break;
